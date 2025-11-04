692389651ffefbd91097017e85a37035d911a4a9
Abhinashrajput447 <2500030978@kluniversity.in>
2025-11-04 21:35:21 +0530

    initial commit

--- Heart-tral-animation/script.js
+++ Heart-tral-animation/script.js
@@ -1,27 +1,40 @@
 const bodyEl = document.querySelector("body");
 
-bodyEl.addEventListener("mousemove", (event) => {
-  const xPos = event.pageX;
-  const yPos = event.pageY;
-
+// Function to create heart
+function createHeart(x, y) {
   const spanEl = document.createElement("span");
-  spanEl.style.left = `${xPos}px`;
-  spanEl.style.top = `${yPos}px`;
+  spanEl.style.left = `${x}px`;
+  spanEl.style.top = `${y}px`;
 
-  // random size
-  const size = Math.random() * 60 + 20;
+  // Random size (smaller for mobile)
+  const size = Math.random() * 40 + 20;
   spanEl.style.width = `${size}px`;
   spanEl.style.height = `${size}px`;
 
-  // random hue (color)
+  // Random color hue
   const hue = Math.floor(Math.random() * 360);
   spanEl.style.filter = `hue-rotate(${hue}deg)`;
 
-  // add heart
   bodyEl.appendChild(spanEl);
 
-  // remove after animation ends
+  // Remove after animation
   setTimeout(() => {
     spanEl.remove();
   }, 3000);
+}
+
+// Desktop mouse move
+bodyEl.addEventListener("mousemove", (event) => {
+  createHeart(event.pageX, event.pageY);
+});
+
+// Mobile touch move
+bodyEl.addEventListener("touchmove", (event) => {
+  const touch = event.touches[0];
+  createHeart(touch.pageX, touch.pageY);
+});
+
+// Also create heart on tap/click
+bodyEl.addEventListener("click", (event) => {
+  createHeart(event.pageX, event.pageY);
 });
